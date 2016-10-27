ba08874fc0a078474e526dd4d0dbc3d5b0453d81
Jan Berges <jan.berges@uni-bremen.de>
2016-10-27 08:46:17 +0200

    Create slides with some of the single-band results

--- scripts/self-energy.py
+++ scripts/self-energy.py
@@ -58,11 +58,19 @@ plot.zlabel = r'$T / T \sub c$'
 
 plot.zticks = [0.6, 0.8, 1.0]
 
+plot_slides = storylines.Plot(9.8, 4.0, margin=0)
+
+plot_slides.xlabel = plot.xlabel
+plot_slides.zlabel = plot.zlabel
+
+plot_slides.zticks = plot.zticks
+
 for key, plot.ylabel in [
     ('Delta', r'$\Delta(\I \omega) / \unit{meV}$'),
     ('chi', r'$\chi(\I \omega) / \unit{meV}$'),
     ('Z', r'$Z(\I \omega)$'),
     ]:
+    plot.clear()
 
     for results in data if key == 'Delta' else [data[i] for i in 0, -1]:
         selections = [np.where(results['iomega'] <= cutoff) for cutoff in 3, 15]
@@ -72,13 +80,19 @@ for key, plot.ylabel in [
                 mark='*', mark_size='0.8pt', only_marks=True, omit=False)
 
     plot.save('../results/self-energy-%s-im.sl' % key.lower(), external=True)
-    plot.clear()
+
+    plot_slides.ylabel = plot.ylabel
+
+    plot_slides.lines  = plot.lines
+    plot_slides.save('../results/self-energy-%s-im-slides.sl' % key.lower(),
+        external=True)
 
 for key, plot.ylabel in [
     ('Delta', r'$\Delta(\omega) / \unit{meV}$'),
     ('chi', r'$\chi(\omega) / \mathrm{meV}$'),
     ('Z', r'$Z(\omega)$'),
     ]:
+    plot.clear()
 
     for label, style in [('Im', 'dotted'), ('Re', 'solid')]:
         plot.line(thick=True, style=style, label=label)
@@ -88,4 +102,12 @@ for key, plot.ylabel in [
                 results['T'], thick=True, style=style)
 
     plot.save('../results/self-energy-%s-re.sl' % key.lower(), external=True)
-    plot.clear()
+
+    plot_slides.ylabel = plot.ylabel
+
+    plot_slides.ymin = -20.1 if key == 'chi' else None
+    plot_slides.ymax =   5.0 if key == 'chi' else None
+
+    plot_slides.lines = plot.lines
+    plot_slides.save('../results/self-energy-%s-re-slides.sl' % key.lower(),
+        external=True)
