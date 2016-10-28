67ea7c5d4359d434a8aff4db00c9130073dd69c7
Jan Berges <jan.berges@uni-bremen.de>
2016-10-28 15:01:36 +0200

    Improve slide on rescaled Coulomb pseudo-potential

---
+++ scripts/rdos.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import storylines
+import numpy as np
+
+plot = storylines.Plot(3, 2, left=0.5, right=0.2, bottom=0.5, top=0.2,
+    xmin=-1.2, xmax=1.2, ymin=0, ymax=1.2)
+
+plot.xticks = [(-1, '$-E \sub F$'), (0, '$\mu_0 = 0$'), (1, '$E \sub F$')]
+plot.yticks = [(0, 0), (1, '$n(\mu_0)$')]
+
+plot.line([-1, 1], [1, 1], yref=0, color='red', fill='red!20')
+
+plot.save('../results/rdos.sl', external=True)
+plot.clear()
+
+plot.clear()
+
+plot.line([-1.2, 1.2], [1, 1], yref=0, draw='none', fill='red!20')
+plot.line([-1.2, 1.2], [1, 1], draw='red')
+
+plot.xticks[0] = (-1.2, '$-\infty$')
+plot.xticks[2] = ( 1.2,  '$\infty$')
+
+plot.save('../results/cdos.sl', external=True)
+plot.clear()
