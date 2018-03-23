dae58206d0e151c180ed13b3dccbe395669c47a5
Jan Berges <jan.berges@uni-bremen.de>
2018-03-23 15:40:29 +0100

    Expand installation script to compile whole thesis

--- make.sh
+++ make.sh
@@ -8,4 +8,25 @@ ebmb=`pwd`
 export PATH=$ebmb:$PATH
 export PYTHONPATH=$ebmb:$PYTHONPATH
 
+cd manual
+pdflatex ebmb
 cd ..
+
+cd ..
+
+cd scripts
+
+for script in *.py
+do
+    echo $script
+    python $script
+done
+
+cd ..
+
+pdflatex -shell-escape thesis
+bibtex8 thesis
+pdflatex thesis
+bibtex8 thesis
+pdflatex thesis
+pdflatex thesis
