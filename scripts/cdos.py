203a2cc3e376c6d9fe7a1da7b9fba0a936b6a9df
Jan Berges <jan.berges@uni-bremen.de>
2021-05-09 17:46:14 +0200

    Remove transparency from CDOS figure

--- scripts/cdos.py
+++ scripts/cdos.py
@@ -20,15 +20,19 @@ plot.ylabel = r'$n(\epsilon) / t^{-1}$'
 
 plot.xticks = [(-4, '$-4 t$'), (mu, '$\mu_0$'), 0, (2, '$2 t$'), (4, '$4 t$')]
 
-style = dict(yref=0, line_join='round', fill_opacity=0.5)
-
 for i, omega in enumerate([1.0, 0.1], 1):
     exact = lorentz(epsilon      - mu, omega) * dos / cdos
     about = lorentz(epsilon_cdos - mu, omega)
 
-    plot.line(epsilon,        dos, fill='black!20', **style)
-    plot.line(epsilon,      exact, fill='red',      **style)
-    plot.line(epsilon_cdos, about, fill='yellow',   **style)
+    style = dict(yref=0, draw='none')
+    plot.line(epsilon,        dos, fill='gray!50',   **style)
+    plot.line(epsilon,      exact, fill='cyan!50',   **style)
+    plot.line(epsilon_cdos, about, fill='orange!50', **style)
+
+    style = dict(yref=0, line_join='round')
+    plot.line(epsilon,        dos, color='gray',   **style)
+    plot.line(epsilon_cdos, about, color='orange', **style)
+    plot.line(epsilon,      exact, color='cyan',   **style)
 
     plot.save('../results/cdos-%d.sl' % i, external=True)
     plot.clear()
